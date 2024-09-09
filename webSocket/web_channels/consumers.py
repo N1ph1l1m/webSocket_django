e9601d8cdc4a80d5f137d9862e2fe5c036f1c0ee
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2024-09-09 21:22:58 +0300

    v.0.4 Create test.py

--- webSocket/web_channels/consumers.py
+++ webSocket/web_channels/consumers.py
@@ -8,6 +8,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        print( 'connect - scope = > ' + self.room_name)
 
         # Получает 'room_name'параметр из маршрута URL, chat/routing.py открывшего соединение WebSocket с потребителем.
         # У каждого потребителя есть область действия , содержащая информацию о его подключении, включая, в частности, любые позиционные или ключевые аргументы из маршрута URL,
@@ -65,30 +66,31 @@ class ChatConsumer(WebsocketConsumer):
         # send message to websocket
         self.send(text_data=json.dumps({"message":message}))
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
-
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
-
-
-
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-
-        await self.channel_layer.group_send(
-            self.room_group_name, {"type":"chat.message","message":message}
-        )
-
-    async def chat_message(self, event):
-        message = event["message"]
-
-        await self.send(text_data=json.dumps({"message":message}))
+# class ChatConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+#         self.room_group_name = f"chat_{self.room_name}"
+#
+#         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+#
+#         await self.accept()
+#
+#
+#     async def disconnect(self, close_code):
+#         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+#
+#
+#
+#     async def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json["message"]
+#
+#         await self.channel_layer.group_send(
+#             self.room_group_name, {"type":"chat.message","message":message}
+#         )
+#
+#     async def chat_message(self, event):
+#         message = event["message"]
+#
+#         await self.send(text_data=json.dumps({"message":message}))
 
